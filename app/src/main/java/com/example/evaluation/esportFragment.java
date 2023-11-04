39aae54195d1f2b0f604d3532dab84199ce72717
TalesReal <tales.palma_2016@outlook.com>
2023-11-04 14:02:56 -0300

    Verção com com gráfico funcionando

--- app/src/main/java/com/example/evaluation/esportFragment.java
+++ app/src/main/java/com/example/evaluation/esportFragment.java
@@ -47,86 +47,93 @@ public class esportFragment extends Fragment {
         WebView webView = view.findViewById(R.id.grafico);
 
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("https://developers.google.com/chart");
-        String htmldata = "<html><body><div id='chard_div'</div></body></html>";
-        webView.loadData(htmldata,"text/html","UTF-8");
-        String codigoJs="<html>\n" +
-                "  <head>\n" +
-                "    <!--Load the AJAX API-->\n" +
+
+        String codigoJs = "\n" +
+                "<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<head>\n" +
                 "    <script type=\"text/javascript\" src=\"https://www.gstatic.com/charts/loader.js\"></script>\n" +
                 "    <script type=\"text/javascript\">\n" +
-                "\t\t\n" +
-                "      // Load the Visualization API and the corechart package.\n" +
-                "      google.charts.load('current', {'packages':['corechart']});\n" +
-                "\n" +
-                "      // Set a callback to run when the Google Visualization API is loaded.\n" +
-                "      google.charts.setOnLoadCallback(drawChart);\n" +
-                "\n" +
-                "      // Callback that creates and populates a data table,\n" +
-                "      // instantiates the pie chart, passes in the data and\n" +
-                "      // draws it.\n" +
-                "      function drawChart() {\n" +
-                "\n" +
-                "        // Create the data table.\n" +
-                "        var data = new google.visualization.DataTable();\n" +
-                "        data.addColumn('string', 'Topping');\n" +
-                "        data.addColumn('number', 'Slices');\n" +
-                "        data.addRows([\n" +
-                "          ['Mushrooms', 3],\n" +
-                "          ['Onions', 1],\n" +
-                "          ['Olives', 1],\n" +
-                "          ['Zucchini', 1],\n" +
-                "          ['Pepperoni', 2]\n" +
-                "        ]);\n" +
-                "\n" +
-                "        // Set chart options\n" +
-                "        var options = {'title':'How Much Pizza I Ate Last Night',\n" +
-                "                       'width':400,\n" +
-                "                       'height':300,\n" +
-                "                      'backgroundColor': '#5271ff'\n" +
+                "        google.charts.load('current', {'packages':['corechart']});\n" +
+                "        google.charts.setOnLoadCallback(drawChart);\n" +
+                "        var data;\n" +
+                "        var options = {\n" +
+                "            width: 415,\n" +
+                "            height:184,\n" +
+                "            backgroundColor: '#82a2ff', // Cor de fundo mais clara\n" +
+                "            titleTextStyle: {\n" +
+                "                color: '#FFFFFF'\n" +
+                "            },\n" +
+                "            hAxis: {\n" +
+                "                textStyle: {\n" +
+                "                    color: '#FFFFFF'\n" +
+                "                }\n" +
+                "            },\n" +
+                "            vAxis: {\n" +
+                "                textStyle: {\n" +
+                "                    color: '#FFFFFF'\n" +
+                "                },\n" +
+                "                gridlines: {\n" +
+                "                    color: 'transparent'\n" +
+                "                }\n" +
+                "            },\n" +
+                "            annotations: {\n" +
+                "                textStyle: {\n" +
+                "                    color: '#FFFFFF'\n" +
+                "                }\n" +
+                "            },\n" +
+                "            series: {\n" +
+                "                0: { color: '#FF5656' },        // Supino - Vermelho\n" +
+                "                1: { color: '#fff90c' },      // Terra - Verde\n" +
+                "                2: { color: '#00ff58' }        // Agachamento - Azul\n" +
+                "            }\n" +
                 "        };\n" +
-                "\n" +
-                "        // Instantiate and draw our chart, passing in some options.\n" +
-                "        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));\n" +
-                "        chart.draw(data, options);\n" +
-                "      }\n" +
-                "    </script>  \n" +
-                "\n" +
-                "\n" +
-                "  <style>\n" +
-                "  body{\n" +
-                "    background-color:#5271ff;\n" +
-                "  }\n" +
-                "  </style>\n" +
-                " \n" +
-                "\n" +
-                "\n" +
-                "\n" +
-                "  </head>\n" +
-                "  \n" +
-                "  <body>\n" +
-                "    <!--Div that will hold the pie chart-->\n" +
+                "        function drawChart() {\n" +
+                "            data = new google.visualization.DataTable();\n" +
+                "            data.addColumn('string', 'Category');\n" +
+                "            data.addColumn('number', 'Supino');\n" +
+                "            data.addColumn('number', 'Terra');\n" +
+                "            data.addColumn('number', 'Agachamento');\n" +
+                "            var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));\n" +
+                "            chart.draw(data, options);\n" +
+                "        }\n" +
+                "        function addDataToChart(supino, terra, agachamento) {\n" +
+                "            data.addRow(['', supino, terra, agachamento]);\n" +
+                "            var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));\n" +
+                "            chart.draw(data, options);\n" +
+                "        }\n" +
+                "    </script>\n" +
+                "<style>"+
+                "body{ background-color:#5271ff;}"+
+                "</style>"+
+                "</head>\n" +
+                "<body>\n" +
                 "    <div id=\"chart_div\"></div>\n" +
-                "  </body>\n" +
+                "</body>\n" +
                 "</html>";
-        webView.loadDataWithBaseURL(null,codigoJs,"text/html","UTF-8",null);
 
+        webView.loadDataWithBaseURL(null, codigoJs, "text/html", "UTF-8", null);
 
-
-
-        view.findViewById(R.id.buttoSalvar).setOnClickListener(v->{
+        view.findViewById(R.id.buttoSalvar).setOnClickListener(v -> {
             Toast toast;
             String input1 = editText1.getText().toString();
             String input2 = editText2.getText().toString();
             String input3 = editText3.getText().toString();
 
-            if(!input1.isEmpty() && !input2.isEmpty() && !input3.isEmpty()){
-                toast = Toast.makeText(this.getContext(),"Informações salvar com sucesso!!",Toast.LENGTH_SHORT);
-            }else{
-                toast = Toast.makeText(this.getContext(),"Complete todos os campos por favor!!",Toast.LENGTH_SHORT);
+            if (!input1.isEmpty() && !input2.isEmpty() && !input3.isEmpty()) {
+                webView.loadUrl("javascript:document.getElementById('dataDisplay').innerHTML = " +
+                        "'Dados: " + input1 + ", " + input2 + ", " + input3 + "';");
+
+                webView.loadUrl("javascript:addDataToChart(" + input1 + ", " + input2 + ", " + input3 + ");");
+
+                toast = Toast.makeText(requireContext(), "Gráfico atualizado com sucesso!!", Toast.LENGTH_SHORT);
+            } else {
+                toast = Toast.makeText(requireContext(), "Complete todos os campos por favor!!", Toast.LENGTH_SHORT);
             }
             toast.show();
         });
+
+
 
 
 
